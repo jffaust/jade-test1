7794685fa4035579ce84403295487814b579963a
Jean-Frédéric Faust <4367222+jffaust@users.noreply.github.com>
2026-02-27 10:23:07 -0500

    add config

---
+++ .config/config.ts
@@ -0,0 +1,6 @@
+export default {
+  // The base title of the site which will also show up in the browser tab
+  title: "My Site",
+  // The description of the site which is included in the meta tag
+  description: "This is a sample site",
+};
